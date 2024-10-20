82939bdf0422a9cac773a00ad70aa7857184a848
amritsapkotadev <amritsapkota.dev@gmail.com>
2024-10-20 17:25:59 +0545

    model added

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
+const { name } = require('ejs');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/testapp1');
 const userschema = mongoose.Schema({
+    name: String,
     image: String,
     password: String,
     email: String,
